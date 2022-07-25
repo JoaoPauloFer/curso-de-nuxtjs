d95edad17bd57b0e3036313e08826979a278e7c9
João Paulo Fer <joaopaulo.data@gmail.com>
2022-07-25 10:27:20 -0300

    docs: 📑  Trabalhando com Layouts

--- pages/a-empresa.vue
+++ pages/a-empresa.vue
@@ -1,19 +1,7 @@
 <template>
     <div>
-        <div class="bg-gray-100">
-            <div class="container mx-auto flex items-center space-x-4 h-10 mb-10">
-                <NuxtLink :to="{ name: 'a-empresa' }">A empresa</NuxtLink>
-                <NuxtLink to="/servicos">Serviços</NuxtLink>
-                <NuxtLink to="/produtos">Produtos</NuxtLink>
-            </div>
-        </div>
-
-        <div>
-            <div class="container mx-auto">
-                <h1 class="text-lg font-bold">A empresa</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente repellendus ad eaque, praesentium aliquam odio quia et, laboriosam sint minus nostrum omnis tempora quis. Minus ad dicta saepe mollitia nesciunt.</p>
-            </div>
-        </div>
+            <h1 class="text-lg font-bold">A empresa</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente repellendus ad eaque, praesentium aliquam odio quia et, laboriosam sint minus nostrum omnis tempora quis. Minus ad dicta saepe mollitia nesciunt.</p>
     </div>
 </template>
 
--- pages/produtos/index.vue
+++ pages/produtos/index.vue
@@ -1,13 +1,5 @@
 <template>
     <div>
-        <div class="bg-gray-100">
-            <div class="container mx-auto flex items-center space-x-4 h-10 mb-10">
-                <NuxtLink :to="{ name: 'a-empresa' }">A empresa</NuxtLink>
-                <NuxtLink to="/servicos">Serviços</NuxtLink>
-                <NuxtLink to="/produtos">Produtos</NuxtLink>
-            </div>
-        </div>
-
         <div>
             <div class="container mx-auto">
                 <h1 class="text-lg font-bold">Produtos</h1>
