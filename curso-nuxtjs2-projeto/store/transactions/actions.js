5ae6c291e9496d271948ada8c3c6b2d37b5603ef
João Paulo Fer <joaopaulo.data@gmail.com>
2022-07-29 13:14:35 -0300

    docs: 📑  Listando as transações

--- curso-nuxtjs2-projeto/store/transactions/actions.js
+++ curso-nuxtjs2-projeto/store/transactions/actions.js
@@ -1,5 +1,12 @@
 export default {
+
+    getTransactions() {
+        return this.$axios.$get('transactions?_expand=category');
+    },
+
     addTransaction(context, data) {
         return this.$axios.$post('transactions' , data )
-    }
+    },
+
+
 }
